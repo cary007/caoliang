997564d3fcfe0c183dbf4d0ef97440bad77fec58
cary007 <45548986+cary007@users.noreply.github.com>
2019-01-11 17:40:34 +0800

    test.c
    test  fasfsf

---
+++ main_tst.c
@@ -0,0 +1,17 @@
+int main(int argc, char **argv)
+{
+	int ret;
+
+	nvme.extensions->parent = &nvme;
+	if (argc < 2) {
+		general_help(&builtin);
+		return 0;
+	}
+	setlocale(LC_ALL, "");
+
+	ret = handle_plugin(argc - 1, &argv[1], nvme.extensions);
+	if (ret == -ENOTTY)
+    general_help(&builtin);
+
+	return ret;
+}
